52508e5122bf6b2e4a9cff6152ad908660220d9d
priyankang <priyankak18@navgurukul.org>
2021-06-16 14:38:10 +0530

    There are JS questions

---
+++ FUNCTION/express_fun.js
@@ -0,0 +1,7 @@
+var funcName = function (num1, num2) {
+    if(num1===num2){
+        return 'yess';
+    }
+    return 'NO';
+}
+console.log(funcName('rinki','Rinki'));
---
+++ FUNCTION/self_invoke.js
@@ -0,0 +1,14 @@
+(function (arr1, arr2){
+    if(arr1 < arr2){
+        console.log(arr2);
+    }else{
+        console.log(arr1);
+    }
+})(34,8);
+
+
+
+
+// (function(str1,str2){
+//     console.log(str1===str2)
+//  })("kumar","kumar");
